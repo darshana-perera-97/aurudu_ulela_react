8825cc2f85163fd6959bc59b9391c13bb6f5adef
Darshana Perera <ds.perera.1997@gmail.com>
2023-04-01 08:34:48 +0530

    animated sun added

--- src/Design/Common/MainHeading.js
+++ src/Design/Common/MainHeading.js
@@ -1,10 +1,21 @@
 import React from "react";
 import wish_01 from "../images/wish_01.png";
+import sun from "../images/sun.png";
 
 export default function MainHeading() {
   return (
     <div className="main-heading">
-      <img src={wish_01} className="main_wish" />
+      <img
+        src={wish_01}
+        className="main_wish"
+        alt="Main wishing text is here"
+          />
+      <img
+        src={sun}
+        className="main_wish sun"
+        alt="Main wishing text is here"
+          />
+          
     </div>
   );
 }
